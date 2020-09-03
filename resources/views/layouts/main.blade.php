8ee684f3e06c87a46688fadcc13c7d06a9061933
Putra Pratama Nasution <putrapratamanst@gmail.com>
2020-09-04 03:00:43 +0700

    fix templating layout

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,19 @@
+@include('layouts.header')
+
+<body>
+    
+    <div class="row">
+        <div class="col-md-2">
+        </div>
+        <div class="col-md-8 middle-content">
+
+            <!-- /.content -->
+            @yield('content')
+            <!-- /.content -->
+        </div>
+        <div class="col-md-2">
+        </div>
+    </div>
+</body>
+
+</html>
