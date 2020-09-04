73d83d72b4c5337408769310d290816024532a82
Putra Pratama Nasution <putrapratamanst@gmail.com>
2020-09-04 16:42:47 +0700

    fix loading screen landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,21 @@
 @extends('layouts.main')
 @section('content')
 
+
+<div class="preloader">
+    <div class="loading">
+        <img src="{{ asset('assets/images/logo.gif') }}">
+    </div>
+</div>
+
 <h3 style="text-align: center;">Stickearn Technical Test </h3>
 <h5 style="text-align: center;">(Word Scrambler)</h5>
 
 <p style="text-align: center;">
-    <img src=" {{ asset('assets/images/logo.jpg') }}" width="350" height="350" ">
+    <img src="{{ asset('assets/images/logo.jpg') }}" width="350" height="350" ">
     <span style=" font-family: 'Nunito Sans' ;font-size: 22px;"> Click <a href="/player/create" class="btn btn-dark blink">PLAY</a> To Start the Game</span>
+
 </p>
+
 
 @endsection
