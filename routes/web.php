85c50a898d97371cda143a32fb4f743b1ef5137a
Putra Pratama Nasution <putrapratamanst@gmail.com>
2020-09-05 01:29:20 +0700

    add route for show page playground

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/player/form', 'PlayerController@form');
 Route::post('/player/create', 'PlayerController@create');
+Route::get('/scramble/playground', 'ScramblerController@playground');
