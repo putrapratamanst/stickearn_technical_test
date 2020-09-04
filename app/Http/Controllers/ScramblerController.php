3cc27f974071a5395ee3d1fa2c290675e5c6ce32
Putra Pratama Nasution <putrapratamanst@gmail.com>
2020-09-05 02:50:23 +0700

    generate and check endpoint

--- app/Http/Controllers/ScramblerController.php
+++ app/Http/Controllers/ScramblerController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Repository\PlayerRepository;
+use App\Repository\ScoreRepository;
 use Illuminate\Http\Request;
 
 class ScramblerController extends Controller
@@ -23,13 +25,36 @@ class ScramblerController extends Controller
      */
     public function playground(Request $request)
     {
+        $scoreRepo  = new ScoreRepository();
+        $playerRepo = new PlayerRepository();
         $dataPlayer = [
             'username' => $request->session()->get('username'),
             'password' => $request->session()->get('password'),
         ];
 
-        return view('/scramble/playground', [
-            'dataPlayer' => $dataPlayer
+        $dataPlayer = $playerRepo->detailPlayer($dataPlayer['username'], $dataPlayer['password']);
+        $score = 0;
+        $scorePlayer = $scoreRepo->scoreByPlayerId($dataPlayer->id);
+        if($scorePlayer)
+            $score = $scorePlayer->score;
+
+        return view('/scrambler/playground', [
+            'dataPlayer' => $dataPlayer,
+            'score'     => $score
         ]);
+    }
+
+    public function check(Request $request)
+    {
+        echo "<pre>";
+        die(print_r($request->all()));
+    }
+
+    public function generate(Request $request)
+    {
+        return response()->json(array('word' => "INI WORD"), 200);
+
+        echo "<pre>";
+        die(print_r($request->all()));
     }
 }
