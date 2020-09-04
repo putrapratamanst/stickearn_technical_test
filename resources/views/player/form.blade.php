2de9f7d05777ca098519a695cc7d86ac0e969621
Putra Pratama Nasution <putrapratamanst@gmail.com>
2020-09-04 17:59:35 +0700

    fix form create player

--- resources/views/player/form.blade.php
+++ resources/views/player/form.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.main')
+
 @section('content')
-{{ Form::open(array('url' => 'foo/bar')) }}
-//
-{{ Form::close() }}@endsection
+
+{{ Form::open(array('url' => 'player/create')) }}
+
+<div id="form-player">
+    <center>Please input username
+        <br>
+        {{ Form::text('username', '', ['required']) }}
+
+        <br>
+        {{ Form::submit('Submit', ['class' => 'btn btn-primary', 'style' => 'margin:10px']) }}
+    </center>
+</div>
+
+
+{{ Form::close() }}
+
+@endsection
