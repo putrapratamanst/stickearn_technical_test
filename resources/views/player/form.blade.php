c20ff6700469e644f1eee28ae19a7de03601cbe8
Putra Pratama Nasution <putrapratamanst@gmail.com>
2020-09-05 01:29:49 +0700

    fix show error message form register user

--- resources/views/player/form.blade.php
+++ resources/views/player/form.blade.php
@@ -2,15 +2,29 @@
 
 @section('content')
 
+
 {{ Form::open(array('url' => 'player/create')) }}
 
-<div id="form-player">
-    <center>Please input username
-        <br>
-        {{ Form::text('username', '', ['required']) }}
+<center>
+    <h2>Login/Register Game Player</h2>
+</center>
+<hr>
+@if ($errors->any())
+<div class="alert alert-danger" style="text-align: center;">
+    <ul>
+        @foreach ($errors->all() as $error)
+        {{ $error }}
+        @endforeach
+    </ul>
+</div>
+@endif
 
-        <br>
-        {{ Form::submit('Submit', ['class' => 'btn btn-primary', 'style' => 'margin:10px']) }}
+<div id="form-player">
+    <center>
+        {{ Form::text('username', '', ['required',  'placeholder' => 'Please input username','class' => 'form-input']) }}
+        {{ Form::password('password', ['required',  'placeholder' => 'Please input password','class' => 'form-input']) }}
+
+        {{ Form::submit('Submit', ['class' => 'btn btn-stickearn', 'style' => 'margin:10px']) }}
     </center>
 </div>
 
