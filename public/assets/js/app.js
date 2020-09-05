ba9f95d6d9227f9dea140cf551e8f59ceda98686
Putra Pratama Nasution <putrapratamanst@gmail.com>
2020-09-05 13:26:12 +0700

    handle ajax for generate word, check word and get score

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,15 +2,51 @@ $(document).ready(function () {
     $(".preloader").fadeOut(5500);
 })
 
-$(document).ready(function () {
-    $.ajax({
-        url: "/scrambler/generate",
-        method:"get",
-        success: function(responseGenerate){
-            $('span#scramble-word').text("("+responseGenerate.word+")");
-        }
-    })
 
+var original = "";
+$(document).ready(function () {
+
+    $("#next").click(function () {
+        $('.form-guess').addClass('d-none');
+        $('.form-player').removeClass('d-none');
+        document.getElementById("form-playground").reset();
+        generate()
+    });
+
+
+    function clearConsole() {
+        if (window.console || window.console.firebug) {
+            console.clear();
+        }
+    }
+
+    function generate() {
+        $.ajax({
+            url: "/scrambler/generate",
+            method: "get",
+            success: function (responseGenerate) {
+                original = responseGenerate.original_word;
+                $('span#scramble-word').text("(" + responseGenerate.scramble_word + ")");
+                clearConsole()
+            }
+        })
+    }
+
+    function score() {
+        $.ajax({
+            url: "/score/get",
+            method: "get",
+            success: function (responseGenerate) {
+                original = responseGenerate.original_word;
+                $('span#score').text(responseGenerate);
+                clearConsole()
+            }
+        })
+    }
+    if (window.location.href.indexOf("playground") > -1) {
+        generate()
+        score()
+    }
 
     $('#guess').click(function (e) {
         e.preventDefault();
@@ -21,27 +57,29 @@ $(document).ready(function () {
             }
         });
 
-        $('#guess').html('Sending..');
-
         /* Submit form data using ajax*/
         $.ajax({
             url: "/scrambler/check",
             method: 'post',
-            data: $('#form-playground').serialize(),
+            data: {
+                original_word: original,
+                form : $('#form-playground').serialize(),
+            },
+
             success: function (response) {
                 //------------------------
-                $('#guess').html('Submit');
                 $('#res_message').show();
-                $('#res_message').html(response.msg);
-                $('#msg_div').removeClass('d-none');
+                $('span#res_message').text(response.message);
+                $('.form-guess').removeClass('d-none');
+                $('.form-player').addClass('d-none');
 
-                document.getElementById("form-playground").reset();
-                setTimeout(function () {
-                    $('#res_message').hide();
-                    $('#msg_div').hide();
-                }, 10000);
+                // document.getElementById("form-playground").reset();
+                // setTimeout(function () {
+                //     $('#form-guess').hide();
+                //     $('#form-player').show();
+                // }, 10000);
                 //--------------------------
-            }
+            },
         });
     });
 });
