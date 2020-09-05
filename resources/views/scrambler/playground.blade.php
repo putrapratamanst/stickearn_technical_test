1c2f579ee47b62ce486d3e5a2b432bb11973f1a4
Putra Pratama Nasution <putrapratamanst@gmail.com>
2020-09-05 15:33:19 +0700

    switch tag html to include

--- resources/views/scrambler/playground.blade.php
+++ resources/views/scrambler/playground.blade.php
@@ -7,26 +7,7 @@
         <img src="{{ asset('assets/images/logo.gif') }}">
     </div>
 </div>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:navy">
-                    <img src="{{ asset('assets/images/logo.jpg') }}" width="30" height="30" class="d-inline-block align-top" alt="">
-                    MENU
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">History Game</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="/" onclick="return confirm('Are you sure to quit the game?')">Quit Game</a>
-                </div>
-            </li>
-        </ul>
-    </div>
-</nav>
-
+@include('layouts.navbar')
 <div class="gameArea">
     <h3 style="color:navy">
         SCORE:<span id="score"></span>
