a05a5c7a80481eceda5013acbb573e858dd39e2d
Putra Pratama Nasution <putrapratamanst@gmail.com>
2020-09-05 14:34:18 +0700

    save score

--- app/Repository/ScoreRepository.php
+++ app/Repository/ScoreRepository.php
@@ -6,15 +6,30 @@ use App\Models\Score;
 
 class ScoreRepository
 {
-    public static function scoreByPlayerId($idPlayer)
+    public static function scoreByPlayerId($playerId)
     {
-        return Score::where('player_id', $idPlayer)
+        return Score::where('player_id', $playerId)
             ->first();
     }
 
-    public static function saveScore()
+
+    public static function saveScore($playerId, $status)
     {
-        $model = new Score();
-        
+        $score = $status == true ? 1 : -1;
+        $model             = new Score();
+        $model->player_id  = $playerId;
+        $model->score      = $score;
+
+        $model->save();
+    }
+
+    public static function updateScore($playerId, $status)
+    {
+        $detail = self::scoreByPlayerId($playerId);
+        $score  = $status == true ? $detail['score']+1 : $detail['score']+(-1);
+        $detail->score = $score;
+        $detail->save();
+
+
     }
 }
