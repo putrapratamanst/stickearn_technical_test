3ffa0d514fa8efa734389cdfd9f550b717a1ae8c
Putra Pratama Nasution <putrapratamanst@gmail.com>
2020-09-05 13:29:07 +0700

    result model

---
+++ app/Models/Result.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Result extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'result';
+
+    /**
+     * Scenario Validation Model
+     */
+}
